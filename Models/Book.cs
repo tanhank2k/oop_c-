e38220cb5f8f95993b6935c92fae783aebcd2ba8
tanhank2k <tanhank2k@gmail.com>
2021-03-21 23:48:28 +0700

    create repo

---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+namespace BookMan.Models
+{
+    public class Book
+    {
+        public int Id;
+        public string Authors;
+        public string Title;
+        public string Publisher;
+        public int _Year;
+        public int Edition;
+        public string Isbn;
+        public string Tags;
+        public string Description;
+        public string File;
+        public bool Reading;
+        public int Rating;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
